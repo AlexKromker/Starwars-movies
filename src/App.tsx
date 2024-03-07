e14c2e3bf4903d9be23e24b4c807e9788af946b3
Alex Kromker <alex@swoopfunding.com>
2024-03-07 17:11:11 +0200

    Added landing page and loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
+import BB8Loader from "./components/loader";
+import Logo from "./shared/assets/images/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles["App"]}>
+      <img src={Logo} alt="" className={styles["star-wars-logo"]} />
+      <BB8Loader />
     </div>
   );
 }
---
+++ src/components/loader/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./loader.module.scss";
+import BB8Body from "../../shared/assets/images/BB8_body.svg";
+import BB8Head from "../../shared/assets/images/BB8_head.svg";
+
+export default function BB8Loader() {
+  return (
+    <div className={styles["loader-container"]}>
+      <div className={styles["BB-8"]}>
+        <img src={BB8Head} alt="BB-8 head" className={styles.head} />
+        <img src={BB8Body} alt="BB-8 body" className={styles.body} />
+      </div>
+    </div>
+  );
+}
